d284ca8e032ec9e677c0e20eaf5ba3086351c7ec
Will_09 <wbahoque71@gmail.com>
2023-07-12 14:02:41 -0500

    Se termina de organizar el proyecto, y se ven los signals

--- poke-qwik/src/routes/index.tsx
+++ poke-qwik/src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 // import Counter from "~/components/starter/counter/counter";
@@ -7,19 +7,33 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 // import Starter from "~/components/starter/next-steps/next-steps";
 
 export default component$(() => {
+
+  const pokemonId = useSignal(1); // useSignal se usa para mantener el estado, se usa con primitivos 
+
   return (
     <>
-      <span class='text-5xl'>Hola Mundo</span>
+      
+      <span class="text-2xl" >Buscador simple</span>
+      
+      <span class="text-9xl" >{ pokemonId }</span>
+
+      {/* TODO: crear imagen */}
+
+      <div class="mt-2">
+        <button class="btn btn-primary mr-2">Anterior</button>
+        <button class="btn btn-primary">Siguiente</button>
+      </div>
+      
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "PokeQwik",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Este es mi primera aplicacion en qwik",
     },
   ],
 };
