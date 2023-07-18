8f8d085632c7bfdecab8f7c7acf87bd4c087f039
Will_09 <wbahoque71@gmail.com>
2023-07-18 16:54:47 -0500

    Se realiza la parte de peticion HTTP

--- poke-qwik/src/routes/pokemons/list-ssr/index.tsx
+++ poke-qwik/src/routes/pokemons/list-ssr/index.tsx
@@ -1,8 +1,57 @@
 import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { Link, type DocumentHead, routeLoader$ } from '@builder.io/qwik-city';
+
+export const usePokemonList = routeLoader$(async() => {
+
+    const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=10`);
+    const data = await resp.json();
+
+    console.log(data);
+
+    return data;
+
+})
 
 export default component$(() => {
-    return <>Hola Mundo - SSR!</>
+
+    const pokemonResp = usePokemonList();
+
+    return (
+        <>
+
+            <div class="flex flex-col">
+                <span class="my-5 text-5xl">Status</span>
+                <span>Pagina actual: xxxx</span>
+                <span>Esta cargando pagina: xxxx</span>
+
+            </div>
+
+            <div class="mt-10">
+                <Link class="btn btn-primary mr-2">
+                    Anteriores
+                </Link>
+
+                <Link class="btn btn-primary mr-2">
+                    Siguientes
+                </Link>
+            </div>
+
+            <div class="grid grid-cols-6 mt-5">
+                <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
+                <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
+                <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
+                <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
+                <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
+                <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
+                <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
+            </div>
+
+            <div>
+                { JSON.stringify(pokemonResp.value) }
+            </div>
+
+        </>
+    )
 });
 
 export const head: DocumentHead = {
