d022ac22065b3029f159f6f6eddf1074bd0da5fc
Will_09 <wbahoque71@gmail.com>
2023-07-18 17:58:19 -0500

    Se empienzan a validar los querys parameters

--- poke-qwik/src/routes/pokemons/list-ssr/index.tsx
+++ poke-qwik/src/routes/pokemons/list-ssr/index.tsx
@@ -1,20 +1,24 @@
 import { component$ } from '@builder.io/qwik';
-import { Link, type DocumentHead, routeLoader$ } from '@builder.io/qwik-city';
+import { Link, type DocumentHead, routeLoader$, useLocation } from '@builder.io/qwik-city';
+import type { BasicPokemonInfo, PokemonListResponse } from '~/interfaces';
 
-export const usePokemonList = routeLoader$(async() => {
+export const usePokemonList = routeLoader$<BasicPokemonInfo[]>(async() => {
 
     const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=10`);
-    const data = await resp.json();
+    const data = await resp.json() as PokemonListResponse;
 
     console.log(data);
 
-    return data;
+    return data.results;
 
 })
 
 export default component$(() => {
 
     const pokemonResp = usePokemonList();
+    const location = useLocation();
+
+    console.log(location.url);
 
     return (
         <>
@@ -27,28 +31,34 @@ export default component$(() => {
             </div>
 
             <div class="mt-10">
-                <Link class="btn btn-primary mr-2">
+                <Link href={'/pokemons/list-ssr/?offset=10'} 
+                    class="btn btn-primary mr-2">
                     Anteriores
                 </Link>
 
-                <Link class="btn btn-primary mr-2">
+                <Link href={'/pokemons/list-ssr/?offset=20'} 
+                    class="btn btn-primary mr-2">
                     Siguientes
                 </Link>
             </div>
 
             <div class="grid grid-cols-6 mt-5">
-                <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
-                <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
-                <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
-                <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
-                <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
-                <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
-                <div class="m-5 flex flex-col justify-center items-center">Pokemon</div>
+                {
+                    pokemonResp.value.map(({ name }) => (
+                        <div 
+                            key={ name }
+                            class="m-5 flex flex-col justify-center items-center"
+                        >
+                            <span class="capitalize">{ name }</span>
+                        </div>
+                    ))
+                }
+                
             </div>
 
-            <div>
+            {/* <div>
                 { JSON.stringify(pokemonResp.value) }
-            </div>
+            </div> */}
 
         </>
     )
