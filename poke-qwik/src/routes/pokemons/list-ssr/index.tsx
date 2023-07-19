13153301cced41b4afa25407d794f5aa433f73d1
Will_09 <wbahoque71@gmail.com>
2023-07-19 16:21:29 -0500

    Se termina la peticion y se hace paginacion

--- poke-qwik/src/routes/pokemons/list-ssr/index.tsx
+++ poke-qwik/src/routes/pokemons/list-ssr/index.tsx
@@ -1,13 +1,21 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useComputed$ } from '@builder.io/qwik';
 import { Link, type DocumentHead, routeLoader$, useLocation } from '@builder.io/qwik-city';
 import type { BasicPokemonInfo, PokemonListResponse } from '~/interfaces';
 
-export const usePokemonList = routeLoader$<BasicPokemonInfo[]>(async() => {
+export const usePokemonList = routeLoader$<BasicPokemonInfo[]>(async({query, redirect, pathname}) => {
 
-    const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=10`);
+    // console.log({query,pathname});
+    const offset = Number( query.get('offset') || '0' );
+    if (isNaN(offset)) throw redirect(301, pathname);
+    if (offset < 0) throw redirect(301, pathname);
+    
+    
+
+    const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`);
     const data = await resp.json() as PokemonListResponse;
 
-    console.log(data);
+    // console.log(data);
+    
 
     return data.results;
 
@@ -18,25 +26,35 @@ export default component$(() => {
     const pokemonResp = usePokemonList();
     const location = useLocation();
 
-    console.log(location.url);
+    const currentOffset = useComputed$<number>(() => {
+        // const offsetString = location.url.searchParams.get('offset'); No encuentra el offset
+        const offsetString = new URLSearchParams( location.url.search ); 
+
+        // console.log(offsetString.get('offset'));
+
+
+        return Number(offsetString.get('offset') || 0);
+    })
+
+    // console.log(location.url);
 
     return (
         <>
 
             <div class="flex flex-col">
                 <span class="my-5 text-5xl">Status</span>
-                <span>Pagina actual: xxxx</span>
-                <span>Esta cargando pagina: xxxx</span>
+                <span>Pagina actual: { currentOffset }</span>
+                <span>Esta cargando pagina: { location.isNavigating ? 'Si':'No' }</span>
 
             </div>
 
             <div class="mt-10">
-                <Link href={'/pokemons/list-ssr/?offset=10'} 
-                    class="btn btn-primary mr-2">
+                <Link href={`/pokemons/list-ssr/?offset=${ currentOffset.value - 10 }`} 
+                    class={currentOffset.value >= 10 ? "btn btn-primary mr-2" : "btn btn-block pointer-events-none mr-2"}>
                     Anteriores
                 </Link>
 
-                <Link href={'/pokemons/list-ssr/?offset=20'} 
+                <Link href={`/pokemons/list-ssr/?offset=${ currentOffset.value + 10 }`} 
                     class="btn btn-primary mr-2">
                     Siguientes
                 </Link>
