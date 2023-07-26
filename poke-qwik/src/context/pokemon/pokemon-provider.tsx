99a3830ad9ee49f0e5f9d9e0584efab28a457a47
Will_09 <wbahoque71@gmail.com>
2023-07-26 11:43:34 -0500

    Se guardan los datos del juego de pokemon

--- poke-qwik/src/context/pokemon/pokemon-provider.tsx
+++ poke-qwik/src/context/pokemon/pokemon-provider.tsx
@@ -1,5 +1,5 @@
 
-import { Slot, component$, useContextProvider, useStore } from '@builder.io/qwik';
+import { Slot, component$, useContextProvider, useStore, useVisibleTask$ } from '@builder.io/qwik';
 import { type PokemonGameState, PokemonGameContext } from './pokemon-game-context';
 import { type PokemonListState, PokemonListContext } from './pokemon-list.context';
 
@@ -19,7 +19,16 @@ export const PokemonProvider = component$(() => {
     
     useContextProvider( PokemonGameContext, pokemonGame ); // Este useContextProvider se coloca aqui para tener informacion global del estado en los componentes hijos
     useContextProvider( PokemonListContext, pokemonLoading );
+
+    useVisibleTask$(() => {
+        // TODO: leer del local storage
+    });
     
+    useVisibleTask$(({ track }) => { // Se utiliza este useVisibleTask para guardar datos en el localStorage
+        track( () => [ pokemonGame.pokemonId, pokemonGame.postionImagenPokemon, pokemonGame.revelationImage ] );
+
+        localStorage.setItem('pokemon-game', JSON.stringify(pokemonGame));
+    });
 
     return <Slot />
 
