7e6bbab49ec0cbcd4b86d48cb08feca284253bb0
Will_09 <wbahoque71@gmail.com>
2023-07-27 10:56:51 -0500

    Se termina de armar el custom hoook

--- poke-qwik/src/hooks/use-pokemon-game.tsx
+++ poke-qwik/src/hooks/use-pokemon-game.tsx
@@ -12,10 +12,25 @@ export const usePokemonGame = () => {
         pokemonGame.pokemonId += value;
     })
 
+    const toogleFromBack = $(() => {
+        pokemonGame.postionImagenPokemon = !pokemonGame.postionImagenPokemon
+    });
+
+    const toogleVisible = $(() => {
+        pokemonGame.revelationImage = !pokemonGame.revelationImage
+    });
+
     return {
         pokemonId           : useComputed$(() => pokemonGame.pokemonId),
         postionImagenPokemon: useComputed$(() => pokemonGame.postionImagenPokemon),
         revelationImage     : useComputed$(() => pokemonGame.revelationImage),
+
+        nextPokemon: () => changePokemonId(+1),
+        prevPokemon: () => changePokemonId(-1),
+
+        toogleFromBack: toogleFromBack, // Aqui cambiamos por medio de una funcion la posicion de la imagen
+        toogleVisible : toogleVisible, // Aqui cambiamos por medio de una funcion la visibilidad de la imagen
+
     }
 
 }
