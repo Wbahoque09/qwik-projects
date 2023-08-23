3f349d6a1bad45cadaee5c403c75e2763ab843fa
Will_09 <wbahoque71@gmail.com>
2023-08-23 17:48:32 -0500

    Terminar implementacion del formulario basico

--- poke-qwik/src/routes/(auth)/login/index.tsx
+++ poke-qwik/src/routes/(auth)/login/index.tsx
@@ -1,9 +1,42 @@
-import { component$ } from '@builder.io/qwik';
+import { $, component$, useStore, useStylesScoped$ } from '@builder.io/qwik';
+
+import styles from './login.css?inline';
 
 export default component$(() => {
+
+    useStylesScoped$(styles);
+
+    const formState = useStore({
+        email: '',
+        password: '',
+    });
+
+    const onSubmit = $(() => {
+    
+    });
+
     return (
-        <>
-            <h2>Login Form</h2>
-        </>
+        <form class="login-form">
+            <div class="relative">
+                <input 
+                    onInput$={ (ev) => formState.email = (ev.target as HTMLInputElement).value }
+                    name="email" type="text" placeholder="Email address" />
+                <label for="email">Email Address</label>
+            </div>
+            <div class="relative">
+                <input
+                    onInput$={ (ev) => formState.password = (ev.target as HTMLInputElement).value }
+                    id="password" name="password" type="password" placeholder="Password" />
+                <label for="password">Password</label>
+            </div>
+            <div class="relative">
+                <button>Ingresar</button>
+            </div>
+
+
+            <code>
+                { JSON.stringify( formState, undefined , 2 ) }
+            </code>
+        </form>
     )
 });
